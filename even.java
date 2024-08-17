b48de1aedda5c36f7e7e1fc65e45930594d111fb
arun <govindarunkumar114@gmaail.com>
2024-08-17 16:54:21 +0530

    second commit

---
+++ even.java
@@ -0,0 +1,11 @@
+class Even{
+  public Static void main(String[] args){
+    int n=1;
+    if(n%2==0){
+      System.out.println("even");
+    }
+    else{
+      System.out.println("Odd");
+    }
+  }
+}
